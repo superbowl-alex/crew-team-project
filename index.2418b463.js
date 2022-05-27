1fba1d573184448c241587a0502a5843ada3f4b2
superbowl-alex <71338915+superbowl-alex@users.noreply.github.com>
2022-05-27 19:54:38 +0000

    Deploying to gh-pages from @ superbowl-alex/crew-team-project@06b93de4754603e016ab6a63190441576fcbca5e 🚀

---
+++ index.2418b463.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("header");window.addEventListener("scroll",(function(){0!==window.scrollY?(e.classList.remove("header"),e.classList.add("js-header-scroll")):(e.classList.add("header"),e.classList.remove("js-header-scroll"),console.log(window.scrollY))}));
+//# sourceMappingURL=index.2418b463.js.map
---
+++ index.c033629e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("header");window.addEventListener("scroll",(function(){0!==window.scrollY?(e.classList.remove("header"),e.classList.add("js-header-scroll")):(e.classList.add("header"),e.classList.remove("js-header-scroll"),console.log(window.scrollY))}))}();
+//# sourceMappingURL=index.c033629e.js.map
