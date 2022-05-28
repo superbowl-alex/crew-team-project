121e823cd69b3e78ea368cdd607f4afc98e5bc87
superbowl-alex <71338915+superbowl-alex@users.noreply.github.com>
2022-05-28 10:27:08 +0000

    Deploying to gh-pages from @ superbowl-alex/crew-team-project@28c38b4a1f389c4237cc8e08ab1d9442df3e4d19 🚀

---
+++ index.57faede6.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector(".menu-btn-open"),closeMenuBtn:document.querySelector(".mobile-btn-close"),menu:document.querySelector(".mobile-menu"),body:document.querySelector(".body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.57faede6.js.map
---
+++ index.9ab8624b.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector(".menu-btn-open"),closeMenuBtn:document.querySelector(".mobile-btn-close"),menu:document.querySelector(".mobile-menu"),body:document.querySelector(".body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.9ab8624b.js.map
